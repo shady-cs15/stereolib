5691e540c690232ce6f576e73200809fef9f83b2
Satyaki Chakraborty <satyaki.cs15@gmail.com>
2015-03-11 10:31:12 +0530

    reconstruction not working

--- depth.cpp
+++ depth.cpp
@@ -3,11 +3,26 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include "opencv2/contrib/contrib.hpp"
+#include <pcl/common/common_headers.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/visualization/pcl_visualizer.h>
+#include <boost/thread/thread.hpp>
 #include <stdio.h>
 #include <iostream>
 
 using namespace cv;
 using namespace std;
+
+//Function creates a PCL visualizer, sets the point cloud to view and returns a pointer
+boost::shared_ptr<pcl::visualization::PCLVisualizer> createVisualizer (pcl::PointCloud<pcl::PointXYZRGB>::ConstPtr cloud) {
+    boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer (new pcl::visualization::PCLVisualizer ("3D Viewer"));
+    viewer->setBackgroundColor (0, 0, 0);
+    pcl::visualization::PointCloudColorHandlerRGBField<pcl::PointXYZRGB> rgb(cloud);
+    viewer->addPointCloud<pcl::PointXYZRGB> (cloud, rgb, "reconstruction");
+    viewer->addCoordinateSystem ( 1.0 );
+    viewer->initCameraParameters ();
+    return viewer;
+}
 
 int main(int argc, char* argv[])
 {
@@ -201,8 +216,8 @@ int main(int argc, char* argv[])
         remap(img1, imgU1, map1x, map1y, INTER_LINEAR, BORDER_CONSTANT, Scalar());
         remap(img2, imgU2, map2x, map2y, INTER_LINEAR, BORDER_CONSTANT, Scalar());
 
-        cvtColor(imgU1,img1,CV_BGR2GRAY);
-        cvtColor(imgU2,img2,CV_BGR2GRAY);
+        cvtColor(imgU1,gray1,CV_BGR2GRAY);
+        cvtColor(imgU2,gray2,CV_BGR2GRAY);
 
         if (prefilsztracker<5) prefilsztracker=5;
         BMState.state->preFilterSize=(prefilsztracker%2)?prefilsztracker:prefilsztracker-1;
@@ -230,7 +245,7 @@ int main(int argc, char* argv[])
         cout<<"\n";
         */
         
-        BMState(img1,img2,disp);
+        BMState(gray1,gray2,disp);
         normalize(disp, disp8, 0, 255, CV_MINMAX, CV_8U);
         imshow("Frame 1",img1);
         imshow("Frame 2",img2);
@@ -241,17 +256,82 @@ int main(int argc, char* argv[])
 
     //debugging
     //for displaying disparity map
+    //and 3d reconstruction
 
-    unsigned char *disparity = (unsigned char*)(disp8.data);
+    //extract Q parameters
+    double _cx, _cy, f, _tx_inv, _cx_cx_tx_inv;  //-cx, -cy, f, -1/Tx, (cx - cx')/Tx
+    _cx = -296.15;//Q.at<double>(0,3); 
+    cout<<_cx<<endl;
+    _cy = -233.733;//Q.at<double>(1,3); 
+    cout<<_cy<<endl;
+    f = 564.469;//Q.at<double>(2,3); 
+    cout<<f<<endl;
+    _tx_inv = -0.11341;//Q.at<double>(3,2); 
+    cout<<_tx_inv<<endl;
+    _cx_cx_tx_inv = 4.16586; //Q.at<double>(3,3); 
+    cout<<_cx_cx_tx_inv<<endl;
 
-    int i,j,r,g,b;
+    //unsigned char *disparity = (unsigned char*)(disp8.data);
+    //unsigned char* rgb = (unsigned char*) (img1.data);
+
+    //create and initialize and fill a point cloud
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr point_cloud_ptr (new pcl::PointCloud<pcl::PointXYZRGB>);
+    double px, py, pz;
+    unsigned char pr,pg, pb;
+
+    for (int i=0;i<img1.rows;i++) {
+        unsigned char* rgb = img1.ptr<unsigned char>(i);
+        unsigned char* disparity = disp8.ptr<unsigned char>(i);
+        for (int j=0;j<img1.cols;j++) {
+            unsigned char d = disparity[j];
+            if (d==0) continue;
+            double pw = -1.0*(double) (d)*_tx_inv + _cx_cx_tx_inv;
+            px = (double) j + _cx;
+            py = (double) j + _cy;
+            pz = f;
+
+            px/=pw;
+            py/=pw;
+            pz/=pw;
+
+            pb = rgb[3*j];
+            pg = rgb[3*j+1];
+            pr = rgb[3*j+2];
+
+            pcl::PointXYZRGB point;
+            point.x = px;
+            point.y = py;
+            point.z = pz;
+
+            uint32_t _rgb = ((uint32_t) pr << 16 |
+              (uint32_t) pg << 8 | (uint32_t)pb);
+            point.rgb = *reinterpret_cast<float*>(&_rgb);
+            point_cloud_ptr->points.push_back (point);
+        }
+    }
+    point_cloud_ptr->width = (int) point_cloud_ptr->points.size();
+    point_cloud_ptr->height = 1;
+
+    //create visualizer to display point clouds
+    boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer;
+    viewer = createVisualizer( point_cloud_ptr );
+
+    while ( !viewer->wasStopped()) {
+        viewer->spinOnce(100);
+        boost::this_thread::sleep (boost::posix_time::microseconds (100000));
+    }
+  
+    /*int i,j,r,g,b;
     for(int i = 0;i < disp8.cols;i++){
         for(int j = 0;j < disp8.rows;j++){
             b = disparity[disp8.cols * j + i ] ;
           //  cout<<b<<" ";
         }
         //cout<<"\n";
-    }
+    }*/
+
+    //Mat _3dImage = Mat(480,640,CV_32F);
+    //reprojectImageTo3D(disp8,_3dImage,Q,true);
     //debugging
 
     return 0;
